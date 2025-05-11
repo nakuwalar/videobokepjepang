c8e4ccfcdbb75bcd445dfb36326d4abfd59a687f
nakuwalar <nakuwalar@gmail.com>
2025-05-12 04:13:27 +0700

    Update video-card.tsx

--- components/video-card.tsx
+++ components/video-card.tsx
@@ -10,19 +10,19 @@ const VideoCard = ({ video }: any) => {
         <Card className="border-0 rounded-none md:border-[1px] md:rounded-md transform transition duration-200 md:hover:scale-[101%] md:hover:shadow-lg">
             <div className="relative">
                 <Link
-                    href={`/v/${video.file_code}#${video.title}`}
-                    title={`Watch ${video.title}`}
+                    href={`/v/${video.file_code}`}
+                    title={`Bokep ${video.title}`}
+                    target="blank"
                     >
                 <Thumbnail
-                    single_img={video.single_img}
-                    splash_img={video.splash_img}
+                    thumbnail={video.thumbnail}
                     title={video.title}
                 />
                 </Link>
             </div>
             <CardContent className="p-1.5"><h3>
                 <Link
-                    href={`/v/${video.file_code}#${video.title}`}
+                    href={`/v/${video.file_code}`}
                     className="line-clamp-2 text-sm md:text-md font-semibold hover:text-primary focus:text-primary"
                     title={`Watch ${video.title}`}
                 >
